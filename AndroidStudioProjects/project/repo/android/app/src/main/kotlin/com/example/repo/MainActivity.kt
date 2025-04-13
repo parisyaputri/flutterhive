19f098d383d1694154e41fdbf372dffbe3c43357
parisyaputri <125849529+parisyaputri@users.noreply.github.com>
2025-04-13 16:09:43 +0700

    Delete AndroidStudioProjects/project/repo directory

--- AndroidStudioProjects/project/repo/android/app/src/main/kotlin/com/example/repo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.repo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- AndroidStudioProjects/project/repo/lib/models/enums.dart
+++
@@ -1,5 +0,0 @@
- enum Status{
-  completed,
-   pending,
-   inProgress,
- }
